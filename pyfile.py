cc092fc96c5066004181de2a591a3823204b1d8c
chamlin <Chris.Hamlin@marklogic.com>
2022-12-13 17:43:30 -0500

    file types working

--- pyfile.py
+++ pyfile.py
@@ -1,11 +1,13 @@
 import os
+import re
+import json
 
 class mllog:
 
     def __init__(self, node, path, type='none'):
         self.node = node
         self.path = path
-        # filename, type (error, request, access, unknown, none)
+        # filename, type (file, dir; error, request, access, unknown, none)
         self.file = {'filename': path, 'type': type}
 
     def __str__(self):
@@ -16,7 +18,8 @@ class mllogs:
     def __init__(self, config):
         self.config = config
         self.files = {}
-        self.parse_config (config)
+        self.parse_file_config (config)
+        self.detect_file_types ()
 
     def __str__(self):
         s = ""
@@ -26,7 +29,7 @@ class mllogs:
                 s += "    " + str (file) + "\n"
         return s
 
-    def parse_config (self, config):
+    def parse_file_config (self, config):
         # set up basic
         for node in self.config.keys():
             paths = self.config[node].split(',')
@@ -46,5 +49,39 @@ class mllogs:
                     # get the files and add them too
                     for root, dirs, files in os.walk(path):
                         for filename in files:
-                            self.files[key].append (mllog (node, filename, 'file'))
+                            self.files[key].append (mllog (node, os.path.join(root, filename), 'file'))
+    
+    def detect_file_types(self):
+        error_regex = re.compile ('\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d\.\d+ (Finest|Finer|Fine|Debug|Config|Info|Notice|Warning|Error|Critical|Alert|Emergency): ')
+        access_regex = re.compile ('\S+\s\S+\s\S+\s\[\d+/\w+/\d\d\d\d:\d\d:\d\d:\d\d.*HTTP')
         
+        for key in self.files.keys():
+            for file in self.files[key]:
+                print ('> ' + str(file))
+                if file.file['type'] != 'file': continue
+                with open(file.file['filename'], 'r', encoding='UTF-8') as afile:
+                    print ('    reading ...')
+                    lines = 0
+                    while (line := afile.readline().rstrip()):
+                        print(line)
+                        if lines > 5:
+                            break
+                        elif error_regex.match(line):
+                            file.file['type'] = 'error'
+                            break
+                        elif access_regex.match(line):
+                            file.file['type'] = 'access'
+                            break
+                        else:
+                            try:
+                                s = json.loads (line)
+                                print (s)
+                                if 'time' in s: file.file['type'] = 'request'
+                                break
+                            except Exception:
+                                pass
+                        lines += 1
+                print ('< ' + str(file))
+
+
+
