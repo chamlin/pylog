1cddada34789cc84010a2f7d04a7121fcdf988f4
chamlin <Chris.Hamlin@marklogic.com>
2022-12-12 20:32:45 -0500

    initial file list

--- filetest.py
+++ filetest.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python3
+import os
 
 import pyfile as log
 
--- pyfile.py
+++ pyfile.py
@@ -2,11 +2,11 @@ import os
 
 class mllog:
 
-    def __init__(self, node, path):
+    def __init__(self, node, path, type='none'):
         self.node = node
         self.path = path
         # filename, type (error, request, access, unknown, none)
-        self.file = {'filename': path, 'type': 'none'}
+        self.file = {'filename': path, 'type': type}
 
     def __str__(self):
         return f"file: {self.file['filename']} (type {self.file['type']})"
@@ -33,16 +33,18 @@ class mllogs:
             for path in paths:
                 path = path.strip()
                 key = node + '@' + path
-                if node in self.files:
-                    self.files[key].append(mllog (node, path))
+                # add then expand if needed
+                log = mllog (node, path)
+                if key in self.files:
+                    self.files[key].append(log)
                 else:
-                    self.files[key] = [mllog (node, path)]
-        # now go through and get actual files
-        for key in self.files:
-            print (f"checking {key}:\n")
-            for log in self.files[key]:
-                if os.path.isfile (log.file['filename']):
+                    self.files[key] = [log]
+                if os.path.isfile (path):
                     log.file['type'] = 'file'
-                elif os.path.isdir (log.file['filename']):
+                elif os.path.isdir (path):
                     log.file['type'] = 'dir'
+                    # get the files and add them too
+                    for root, dirs, files in os.walk(path):
+                        for filename in files:
+                            self.files[key].append (mllog (node, filename, 'file'))
         
