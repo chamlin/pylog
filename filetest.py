584717ec8398215313c8e268abe65ab72be93f91
chamlin <Chris.Hamlin@marklogic.com>
2022-12-14 20:29:08 -0500

    started error logs

--- filetest.py
+++ filetest.py
@@ -4,8 +4,8 @@ import os
 import pyfile as log
 
 file_config = {
-    '1': 'node1',
-    '2': 'bingo, testdir'
+    #'1': 'node1',
+    '2': 'testdir/node2'
 }
 logs = log.mllogs (file_config)
 print (logs)
