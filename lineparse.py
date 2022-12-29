a2e2a5aa1eae4ac88870bbbb058c2268e3dd3dbb
chamlin <Chris.Hamlin@marklogic.com>
2022-12-29 18:12:18 -0500

    added Merging

--- lineparse.py
+++ lineparse.py
@@ -1,10 +1,22 @@
+import sys
 import re
 
 
 # individual functions here for extraction
 
+def mergingfun (line):
+    retval = list()
+    groups = (re.findall (r'Merging (\d+) MB from (.*) to (.*), timestamp=(\d+)', line))[0]
+    if len(groups) < 4:
+        return list()
+    else:
+        retval.append ({'event': 'merge-to', 'stand': groups[2], 'size': groups[0], 'timestamp': groups[3]})
+        for stand in re.split ('(?:, | and )', groups[1]):
+            retval.append ({'event': 'merge-from', 'stand': stand})
+        return retval
+
 def memfun (line):
-    retval = {'event-type': 'memory-logging'}
+    retval = {'event': 'memory-logging'}
     extracted = False
     for stat in re.findall (r'(\w+)=(\d+)\((\d+)%\)', line):
         name = stat[0]
@@ -32,12 +44,12 @@ def generalfun (init, line, regex, names):
 # here are the configs to control extraction
 
 # in to extract:  text of line
-# out from extract:  list of dicts, each an event with 'event-type' and some other value(s)
+# out from extract:  list of dicts, each an event with 'event' and some other value(s)
 # tests:  array of test lines to check functioning
 extract_config = {
     'D': [
         { 'starts': 'Deleted ',
-          'general-extract': {'init': {'event-type': 'deleted-stand'}, 'names': ['size', 'rate', 'stand'], 'regex': 'Deleted (\d+) MB at (\d+) MB/sec (.*)'},
+          'general-extract': {'init': {'event': 'deleted-stand'}, 'names': ['size', 'rate', 'stand'], 'regex': 'Deleted (\d+) MB at (\d+) MB/sec (.*)'},
           'tests': ['Deleted 25 MB at 7066 MB/sec /var/opt/MarkLogic/Forests/Meters/00004222']
         }
     ],
@@ -47,20 +59,28 @@ extract_config = {
           'tests': ['Memory size=18727(29%) rss=18353(28%) anon=18209(28%) file=54(0%) forest=938(1%) cache=20480(32%) registry=7(0%)']
         },
         { 'starts': 'Merged ',
-          'general-extract': {'init': {'event-type': 'merged'}, 'names': ['size', 'rate', 'stand'], 'regex': 'Merged (\d+) MB(?: in \d+ sec)? at (\d+) MB/sec to (.*)'},
+          'general-extract': {'init': {'event': 'merged'}, 'names': ['size', 'rate', 'stand'], 'regex': 'Merged (\d+) MB(?: in \d+ sec)? at (\d+) MB/sec to (.*)'},
           'tests': [
               'Merged 6 MB at 25 MB/sec to /var/opt/MarkLogic/Forests/rddr-content-1/00003c60',
               'Merged 51 MB in 2 sec at 25 MB/sec to /var/opt/MarkLogic/Forests/Meters/0000418d'
           ]
+        },
+        { 'starts': 'Merging ',
+          'extract': mergingfun,
+          'tests': [
+            'Merging 83 MB from /var/opt/MarkLogic/Forests/Meters/00004197, /var/opt/MarkLogic/Forests/Meters/00004198, and /var/opt/MarkLogic/Forests/Meters/00004196 to /var/opt/MarkLogic/Forests/Meters/0000419a, timestamp=16488943209770340',
+            'Merging 37 MB from /var/opt/MarkLogic/Forests/Meters/00004182 and /var/opt/MarkLogic/Forests/Meters/00004180 to /var/opt/MarkLogic/Forests/Meters/00004184, timestamp=16488943209770340'
+          ]
         }
+
     ],
     'S': [
         { 'starts': 'Saving ',
-          'general-extract': {'init': {'event-type': 'saving-stand'}, 'names': 'stand', 'regex': 'Saving (.+)'},
+          'general-extract': {'init': {'event': 'saving-stand'}, 'names': 'stand', 'regex': 'Saving (.+)'},
           'tests': ['Saving /var/opt/MarkLogic/Forests/rddr-content-1/00003c5b']
         },
         { 'starts': 'Saved ',
-          'general-extract': {'init': {'event-type': 'saved-stand'}, 'names': ['size', 'rate', 'stand'], 'regex': 'Saved (\d+) MB(?: in \d+ sec)? at (\d+) MB/sec to (.*)'},
+          'general-extract': {'init': {'event': 'saved-stand'}, 'names': ['size', 'rate', 'stand'], 'regex': 'Saved (\d+) MB(?: in \d+ sec)? at (\d+) MB/sec to (.*)'},
           'tests': ['Saved 10 MB at 119 MB/sec to /var/opt/MarkLogic/Forests/rddr-content-1/00003c61']
         }
     ]
@@ -80,7 +100,7 @@ def extract_events (line):
 
     Returns
     -------
-    A list of dictionaries, with event-type and some other value(s)
+    A list of dictionaries, with event and some other value(s)
     ''' 
     retval = list()
     for extracter in extract_config.get (line[0], []):
