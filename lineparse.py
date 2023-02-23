db5b381f5016bf326da7b41553df0ba53a7addf2
chamlin <Chris.Hamlin@marklogic.com>
2023-02-23 15:52:54 -0500

    change post-process to a list of func refs

--- lineparse.py
+++ lineparse.py
@@ -18,9 +18,9 @@ def mergingfun (line):
     if len(groups) < 4:
         return list()
     else:
-        retval.append ({'event': 'merging-to', 'stand': shorten_stand(groups[2]), 'size': groups[0], 'timestamp': groups[3]})
+        retval.append ({'event': 'merging-to', 'stand': groups[2], 'size': groups[0], 'timestamp': groups[3]})
         for stand in re.split ('(?:, | and )', groups[1]):
-            retval.append ({'event': 'merging-from', 'stand': shorten_stand(stand)})
+            retval.append ({'event': 'merging-from', 'stand': stand})
         return retval
 
 def memfun (line):
@@ -64,14 +64,16 @@ extract_config = {
     '~': [
         { 'starts': '~OnDiskStand',
           'general-extract': {'init': {'event': 'on-disk-stand-end'}, 'names': ['stand'], 'regex': '~OnDiskStand (.*)'},
-          'tests': ['~OnDiskStand /var/replica-prod/Forests/P_initial_r11_04/00019082']
+          'tests': ['~OnDiskStand /var/replica-prod/Forests/P_initial_r11_04/00019082'],
+          'post-process': {'stand': [shorten_stand]}
         },
         { 'starts': '~InMemoryStand',
           'general-extract': {'init': {'event': 'in-memory-stand-end'}, 'names': ['stand'], 'regex': '~InMemoryStand (.*?)(, .*)?$'},
           'tests': [
             '~InMemoryStand /Users/chamlin/Library/Application Support/MarkLogic/Data/Forests/Meters/00000fa7, indexes storage used % : rangeIndex=5%, reverseIndex=0%, tripleIndex=100%, geospatialRegionIndex=2%',
             '~InMemoryStand /MarkLogic_Data/Forests/P_smartws_r2_02/0001191c'
-          ]
+          ],
+          'post-process': {'stand': [shorten_stand]}
         }
     ],
     '[': [
@@ -84,7 +86,7 @@ extract_config = {
         { 'starts': 'Deleted ',
           'general-extract': {'init': {'event': 'stand-deleted'}, 'names': ['size', 'rate', 'stand'], 'regex': 'Deleted (\d+) MB at (\d+) MB/sec (.*)'},
           'tests': ['Deleted 25 MB at 7066 MB/sec /var/opt/MarkLogic/Forests/Meters/00004222'],
-          'post-process': {'stand': shorten_stand}
+          'post-process': {'stand': [shorten_stand]}
         }
     ],
     'F': [
@@ -103,7 +105,8 @@ extract_config = {
         { 'starts': 'InMemoryStand ',
           'general-extract': {'init': {'event': 'in-memory-stand'}, 'names': ['stand', 'disk', 'memory', 'list', 'tree', 'rangeIndex', 'reverseIndex', 'tripleIndex', 'geospatialRegionIndex'],
                 'regex': 'InMemoryStand (.*), disk=(\d+)MB, memory=(\d+)MB, list=(\d+)MB, tree=(\d+)MB, rangeIndex=(\d+)MB, reverseIndex=(\d+)MB, tripleIndex=(\d+)MB, geospatialRegionIndex=(\d+)MB'},
-          'tests': ['InMemoryStand /Users/chamlin/Library/Application Support/MarkLogic/Data/Forests/Meters/00000fa9, disk=1MB, memory=457MB, list=48MB, tree=24MB, rangeIndex=16MB, reverseIndex=16MB, tripleIndex=384MB, geospatialRegionIndex=16MB']
+          'tests': ['InMemoryStand /Users/chamlin/Library/Application Support/MarkLogic/Data/Forests/Meters/00000fa9, disk=1MB, memory=457MB, list=48MB, tree=24MB, rangeIndex=16MB, reverseIndex=16MB, tripleIndex=384MB, geospatialRegionIndex=16MB'],
+          'post-process': {'stand': [shorten_stand]}
         },
         { 'starts': 'IndexerEnv::putRangeIndex: XDMP-',
           'general-extract': {'init': {'event': 'put-range-index'}, 'names': ['code'], 'regex': 'XDMP-.* (XDMP-[^:]+)'},
@@ -124,21 +127,24 @@ extract_config = {
           'tests': [
               'Merged 6 MB at 25 MB/sec to /var/opt/MarkLogic/Forests/rddr-content-1/00003c60',
               'Merged 51 MB in 2 sec at 25 MB/sec to /var/opt/MarkLogic/Forests/Meters/0000418d'
-          ]
+          ],
+          'post-process': {'stand': [shorten_stand]}
         },
         { 'starts': 'Merging ',
           'extract': mergingfun,
           'tests': [
             'Merging 83 MB from /var/opt/MarkLogic/Forests/Meters/00004197, /var/opt/MarkLogic/Forests/Meters/00004198, and /var/opt/MarkLogic/Forests/Meters/00004196 to /var/opt/MarkLogic/Forests/Meters/0000419a, timestamp=16488943209770340',
             'Merging 37 MB from /var/opt/MarkLogic/Forests/Meters/00004182 and /var/opt/MarkLogic/Forests/Meters/00004180 to /var/opt/MarkLogic/Forests/Meters/00004184, timestamp=16488943209770340'
-          ]
+          ],
+          'post-process': {'stand': [shorten_stand]}
         }
 
     ],
     'O': [
         { 'starts': 'OnDiskStand',
           'general-extract': {'init': {'event': 'on-disk-stand'}, 'names': ['stand', 'disk', 'edisk', 'memory'], 'regex': 'OnDiskStand (.*), disk=(\d+)MB, edisk=(\d+)MB, memory=(\d+)MB'},
-          'tests': ['OnDiskStand /MarkLogic_Data/Forests/P_initial_p23_04/00058661, disk=201MB, edisk=0MB, memory=16MB']
+          'tests': ['OnDiskStand /MarkLogic_Data/Forests/P_initial_p23_04/00058661, disk=201MB, edisk=0MB, memory=16MB'],
+          'post-process': {'stand': [shorten_stand]}
         }
     ],
     'R': [
@@ -150,11 +156,13 @@ extract_config = {
     'S': [
         { 'starts': 'Saving ',
           'general-extract': {'init': {'event': 'saving-stand'}, 'names': 'stand', 'regex': 'Saving (.+)'},
-          'tests': ['Saving /var/opt/MarkLogic/Forests/rddr-content-1/00003c5b']
+          'tests': ['Saving /var/opt/MarkLogic/Forests/rddr-content-1/00003c5b'],
+          'post-process': {'stand': [shorten_stand]}
         },
         { 'starts': 'Saved ',
           'general-extract': {'init': {'event': 'saved-stand'}, 'names': ['size', 'rate', 'stand'], 'regex': 'Saved (\d+) MB(?: in \d+ sec)? at (\d+) MB/sec to (.*)'},
-          'tests': ['Saved 10 MB at 119 MB/sec to /var/opt/MarkLogic/Forests/rddr-content-1/00003c61']
+          'tests': ['Saved 10 MB at 119 MB/sec to /var/opt/MarkLogic/Forests/rddr-content-1/00003c61'],
+          'post-process': {'stand': [shorten_stand]}
         }
     ]
 }
@@ -187,7 +195,8 @@ def extract_events (line):
             if extract and 'post-process' in extracter:
                 for event in extract:
                     for name in extracter.get('post-process'):
-                        event[name] = extracter['post-process'][name](event[name])
+                        for processor in extracter['post-process'][name]:
+                            event[name] = processor(event[name])
             # TODO stop if continue false and match?
             if extract:
                 retval += extract
